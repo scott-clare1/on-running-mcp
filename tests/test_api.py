a45e39389cdf7a66878688b658b6cfde2eb8151f
Scott Clare <scott_clare1@yahoo.co.uk>
2025-04-09 21:07:25 +0100

    feat: initial commit

---
+++ tests/test_api.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from on_running_mcp.core.api import OnRunningAPI, Params
+from on_running_mcp.core.schemas import Payload, ProductType, Request, Filters, ProductSubType
+
+
+class TestAPI(TestCase):
+
+    def test_create_api_instance(self):
+        """Create an instance of OnRunningAPI."""
+        filters = Filters(
+            gender_filter=None,
+            product_type=ProductType.ACCESSORIES,
+            product_subtype=ProductSubType.SOCKS
+        )
+        params = Params(filters=filters)
+        payload = Payload(requests=[Request(params=str(params))])
+        api = OnRunningAPI(payload=payload)
+        self.assertTrue(isinstance(api.hits, list))
